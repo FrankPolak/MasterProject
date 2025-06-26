af71c497e26c2f92fbc9c765d5db7c81782a03f7
FrankPolak <160171754+FrankPolak@users.noreply.github.com>
2025-06-26 19:20:44 +0100

    Add files via upload

--- Autoencoder/build_model.py
+++ Autoencoder/build_model.py
@@ -31,7 +31,7 @@ class AutoencoderV2(nn.Module):
             nn.Linear(in_features=500, out_features=250),
             nn.ReLU(),
             nn.Linear(in_features=250, out_features=hidden_features),
-            nn.ReLU(),
+            nn.ReLU()
         )
 
         # Decoder
@@ -42,7 +42,7 @@ class AutoencoderV2(nn.Module):
             nn.ReLU(),
             nn.Linear(in_features=500, out_features=1000),
             nn.ReLU(),
-            nn.Linear(in_features=1000, out_features=input_features),
+            nn.Linear(in_features=1000, out_features=input_features)
         ]
 
         self.decoder = nn.Sequential(*decoder_layers)
@@ -83,7 +83,7 @@ class AutoencoderV3(nn.Module):
             nn.Linear(in_features=1000, out_features=500),
             nn.ReLU(),
             nn.Linear(in_features=500, out_features=hidden_features),
-            nn.ReLU(),
+            nn.ReLU()
         )
 
         # Decoder
@@ -95,6 +95,266 @@ class AutoencoderV3(nn.Module):
             nn.Linear(in_features=1000, out_features=2000),
             nn.ReLU(),
             nn.Linear(in_features=2000, out_features=input_features)
+        ]
+
+        self.decoder = nn.Sequential(*decoder_layers)
+
+    def forward(self, x):
+        latent_space = self.encoder(x)  # Extract latent representation
+        reconstructed = self.decoder(latent_space)  # Decode from latent space
+        return reconstructed
+
+    def encode(self, x):
+        """Returns only the latent space representation."""
+        return self.encoder(x)
+
+
+class Autoencoder_1_Layer(nn.Module):
+    """
+    Creates an autoencoder architecture with one hidden layer for a dataset with ~4000 features.
+
+    Args:
+        input_features (int): The number of input/output features.
+        hidden_features (int): The number of features in the latent space.
+
+    Methods:
+        forward(x): Passes input through the encoder and decoder to reconstruct the input.
+        encode(x): Returns the latent space representation of the input data.
+    """
+
+
+    def __init__(self, input_features, hidden_features):
+        super(Autoencoder_1_Layer, self).__init__()
+
+        # Encoder
+        self.encoder = nn.Sequential(
+            nn.Linear(in_features=input_features, out_features=2000),
+            nn.ReLU(),
+            nn.Linear(in_features=2000, out_features=hidden_features),
+            nn.ReLU()
+        )
+
+        # Decoder
+        decoder_layers = [
+            nn.Linear(in_features=hidden_features, out_features=2000),
+            nn.ReLU(),
+            nn.Linear(in_features=2000, out_features=input_features)
+        ]
+
+        self.decoder = nn.Sequential(*decoder_layers)
+
+    def forward(self, x):
+        latent_space = self.encoder(x)  # Extract latent representation
+        reconstructed = self.decoder(latent_space)  # Decode from latent space
+        return reconstructed
+
+    def encode(self, x):
+        """Returns only the latent space representation."""
+        return self.encoder(x)
+
+class Autoencoder_2_Layers(nn.Module):
+    """
+    Creates an autoencoder architecture with two hidden layers for a dataset with ~4000 features.
+
+    Args:
+        input_features (int): The number of input/output features.
+        hidden_features (int): The number of features in the latent space.
+
+    Methods:
+        forward(x): Passes input through the encoder and decoder to reconstruct the input.
+        encode(x): Returns the latent space representation of the input data.
+    """
+
+
+    def __init__(self, input_features, hidden_features):
+        super(Autoencoder_2_Layers, self).__init__()
+
+        # Encoder
+        self.encoder = nn.Sequential(
+            nn.Linear(in_features=input_features, out_features=2000),
+            nn.ReLU(),
+            nn.Linear(in_features=2000, out_features=1000),
+            nn.ReLU(),
+            nn.Linear(in_features=1000, out_features=hidden_features),
+            nn.ReLU()
+        )
+
+        # Decoder
+        decoder_layers = [
+            nn.Linear(in_features=hidden_features, out_features=1000),
+            nn.ReLU(),
+            nn.Linear(in_features=1000, out_features=2000),
+            nn.ReLU(),
+            nn.Linear(in_features=2000, out_features=input_features)
+        ]
+
+        self.decoder = nn.Sequential(*decoder_layers)
+
+    def forward(self, x):
+        latent_space = self.encoder(x)  # Extract latent representation
+        reconstructed = self.decoder(latent_space)  # Decode from latent space
+        return reconstructed
+
+    def encode(self, x):
+        """Returns only the latent space representation."""
+        return self.encoder(x)
+
+
+class Autoencoder_3_Layers(nn.Module):
+    """
+    Creates an autoencoder architecture with two hidden layers for a dataset with ~4000 features.
+
+    Args:
+        input_features (int): The number of input/output features.
+        hidden_features (int): The number of features in the latent space.
+
+    Methods:
+        forward(x): Passes input through the encoder and decoder to reconstruct the input.
+        encode(x): Returns the latent space representation of the input data.
+    """
+
+
+    def __init__(self, input_features, hidden_features):
+        super(Autoencoder_3_Layers, self).__init__()
+
+        # Encoder
+        self.encoder = nn.Sequential(
+            nn.Linear(in_features=input_features, out_features=2000),
+            nn.ReLU(),
+            nn.Linear(in_features=2000, out_features=1000),
+            nn.ReLU(),
+            nn.Linear(in_features=1000, out_features=500),
+            nn.ReLU(),
+            nn.Linear(in_features=500, out_features=hidden_features),
+            nn.ReLU()
+        )
+
+        # Decoder
+        decoder_layers = [
+            nn.Linear(in_features=hidden_features, out_features=500),
+            nn.ReLU(),
+            nn.Linear(in_features=500, out_features=1000),
+            nn.ReLU(),
+            nn.Linear(in_features=1000, out_features=2000),
+            nn.ReLU(),
+            nn.Linear(in_features=2000, out_features=input_features)
+        ]
+
+        self.decoder = nn.Sequential(*decoder_layers)
+
+    def forward(self, x):
+        latent_space = self.encoder(x)  # Extract latent representation
+        reconstructed = self.decoder(latent_space)  # Decode from latent space
+        return reconstructed
+
+    def encode(self, x):
+        """Returns only the latent space representation."""
+        return self.encoder(x)
+
+
+class Autoencoder_4_Layers(nn.Module):
+    """
+    Creates an autoencoder architecture with two hidden layers for a dataset with ~4000 features.
+
+    Args:
+        input_features (int): The number of input/output features.
+        hidden_features (int): The number of features in the latent space.
+
+    Methods:
+        forward(x): Passes input through the encoder and decoder to reconstruct the input.
+        encode(x): Returns the latent space representation of the input data.
+    """
+
+
+    def __init__(self, input_features, hidden_features):
+        super(Autoencoder_4_Layers, self).__init__()
+
+        # Encoder
+        self.encoder = nn.Sequential(
+            nn.Linear(in_features=input_features, out_features=3000),
+            nn.ReLU(),
+            nn.Linear(in_features=3000, out_features=2000),
+            nn.ReLU(),
+            nn.Linear(in_features=2000, out_features=1000),
+            nn.ReLU(),
+            nn.Linear(in_features=1000, out_features=500),
+            nn.ReLU(),
+            nn.Linear(in_features=500, out_features=hidden_features),
+            nn.ReLU()
+        )
+
+        # Decoder
+        decoder_layers = [
+            nn.Linear(in_features=hidden_features, out_features=500),
+            nn.ReLU(),
+            nn.Linear(in_features=500, out_features=1000),
+            nn.ReLU(),
+            nn.Linear(in_features=1000, out_features=2000),
+            nn.ReLU(),
+            nn.Linear(in_features=2000, out_features=3000),
+            nn.ReLU(),
+            nn.Linear(in_features=3000, out_features=input_features)
+        ]
+
+        self.decoder = nn.Sequential(*decoder_layers)
+
+    def forward(self, x):
+        latent_space = self.encoder(x)  # Extract latent representation
+        reconstructed = self.decoder(latent_space)  # Decode from latent space
+        return reconstructed
+
+    def encode(self, x):
+        """Returns only the latent space representation."""
+        return self.encoder(x)
+
+
+
+class Autoencoder_5_Layers(nn.Module):
+    """
+    Creates an autoencoder architecture with two hidden layers for a dataset with ~4000 features.
+
+    Args:
+        input_features (int): The number of input/output features.
+        hidden_features (int): The number of features in the latent space.
+
+    Methods:
+        forward(x): Passes input through the encoder and decoder to reconstruct the input.
+        encode(x): Returns the latent space representation of the input data.
+    """
+
+
+    def __init__(self, input_features, hidden_features):
+        super(Autoencoder_5_Layers, self).__init__()
+
+        # Encoder
+        self.encoder = nn.Sequential(
+            nn.Linear(in_features=input_features, out_features=3300),
+            nn.ReLU(),
+            nn.Linear(in_features=3300, out_features=2600),
+            nn.ReLU(),
+            nn.Linear(in_features=2600, out_features=1900),
+            nn.ReLU(),
+            nn.Linear(in_features=1900, out_features=1200),
+            nn.ReLU(),
+            nn.Linear(in_features=1200, out_features=500),
+            nn.ReLU(),
+            nn.Linear(in_features=500, out_features=hidden_features),
+            nn.ReLU()
+        )
+
+        # Decoder
+        decoder_layers = [
+            nn.Linear(in_features=hidden_features, out_features=500),
+            nn.ReLU(),
+            nn.Linear(in_features=500, out_features=1200),
+            nn.ReLU(),
+            nn.Linear(in_features=1200, out_features=1900),
+            nn.ReLU(),
+            nn.Linear(in_features=1900, out_features=2600),
+            nn.ReLU(),
+            nn.Linear(in_features=2600, out_features=3300),
+            nn.ReLU(),
+            nn.Linear(in_features=3300, out_features=input_features)
         ]
 
         self.decoder = nn.Sequential(*decoder_layers)
